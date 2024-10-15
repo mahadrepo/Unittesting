e3ea0a24cbadda4b2e3c4113858a8a407907685e
mahad nadeem <mahad.nadeem@dotfoods.com>
2024-10-15 13:28:26 +0500

    4

--- SimpleFunctionApp/Program.cs
+++ SimpleFunctionApp/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!2");
         }
 
         public static int AddNumbers(int a, int b)
